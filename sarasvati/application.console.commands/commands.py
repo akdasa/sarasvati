3ccefa660a799c29f971e10fe786f65b32ec5fb8
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-04-07 18:42:04 +0300

    more eye candy

--- sarasvati/application.console.commands/commands.py
+++ sarasvati/application.console.commands/commands.py
@@ -1,13 +1,18 @@
 from api.commands import Command
+import colored
+from colored import stylize
 
 
 class ListCommand(Command):
     def __init__(self, api):
         super().__init__(api)
+        self.__title_style = colored.fg("green")
 
     def execute(self):
         for thought in self._api.database.find(None) or []:
-            print("{}: {}".format(thought.title, thought.description))
+            print(
+                stylize(thought.title, self.__title_style),
+                thought.description)
 
     def revert(self):
         pass
