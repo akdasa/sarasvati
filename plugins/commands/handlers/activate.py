4764ae920f48f5c2e479fc992e58e81bf285bc87
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-06-11 11:34:32 +0300

    activate command refactor

--- plugins/commands/handlers/activate.py
+++ plugins/commands/handlers/activate.py
@@ -1,6 +1,12 @@
 from plugins.commands.commands import ActivateCommand
+from sarasvati.commands import CommandException
 
 
 def activate(api, args):
-    thought = api.find_one_by_title(args.get("arg"))
+    title = args.get("arg") or args.get("title")
+
+    if not title:
+        raise CommandException("No title specified")
+
+    thought = api.find_one_by_title(title)
     api.brain.commands.execute(ActivateCommand(thought))
