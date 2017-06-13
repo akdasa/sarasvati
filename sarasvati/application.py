a5f85760aa31f5ca3bc466d7c347021f21c464f6
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-06-13 14:22:43 +0300

    osx test

--- sarasvati/application.py
+++ sarasvati/application.py
@@ -1,7 +1,9 @@
+from abc import ABCMeta
+
 from sarasvati import get_api
 
 
-class SarasvatiApplication:
+class SarasvatiApplication(metaclass=ABCMeta):
     def __init__(self):
         self._api = get_api()
         self._brain = self._api.open_brain(".")
