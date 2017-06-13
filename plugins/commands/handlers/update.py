dc017e5fc96b68702742a1e0fc4feb7e2c3ebf4e
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-06-13 11:09:07 +0300

    cleanup

--- plugins/commands/handlers/update.py
+++ plugins/commands/handlers/update.py
@@ -5,23 +5,22 @@ from sarasvati.commands import CommandException
 
 def update(api, args):
     arg = args.get("arg")
-    title = args.get("title")
-    desc = args.get("desc")
     active = api.brain.state.active_thought
 
     # validation
-    if not title and not desc:
+    if not (args.title or args.desc):
         raise CommandException("Nothing to do")
-    if not arg and not active:
+    if not (arg or active):
         raise CommandException("No title specified nor activated thought")
 
     # get required data
     thought = api.utilities.find_one_by_title(arg) if arg else active
 
     # execute
-    if title:
-        api.brain.commands.execute(SetTitleCommand(thought, title))
-    if desc:
-        api.brain.commands.execute(SetDescriptionCommand(thought, desc))
+    if args.title:
+        api.execute(SetTitleCommand(thought, args.title))
+    if args.desc:
+        api.execute(SetDescriptionCommand(thought, args.desc))
 
+    # result
     return CommandResult(thought, message="Thought '{}' updated".format(thought.title))
