7a815877ed0de6b7c16f2cc6518bf5732c3932e0
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-08-17 11:13:50 +0300

    api cleanup

--- sarasvati/api/api.py
+++ sarasvati/api/api.py
@@ -32,28 +32,65 @@ class SarasvatiApi(Composite):
 
     @property
     def plugins(self):
+        """
+        Returns plugin component
+        :rtype: SarasvatiPluginsApiComponent
+        :return: Plugins component
+        """
         return self.get_component(SarasvatiPluginsApiComponent.COMPONENT_NAME)
 
     @property
     def serialization(self):
+        """
+        Returns serialization component
+        :rtype: SarasvatiSerializationApiComponent
+        :return: Serialization component
+        """
         return self.get_component(SarasvatiSerializationApiComponent.COMPONENT_NAME)
 
     @property
     def utilities(self):
+        """
+        Returns utilities component
+        :rtype: SarasvatiUtilitiesApiComponent
+        :return: Utilities component
+        """
         return self.get_component(SarasvatiUtilitiesApiComponent.COMPONENT_NAME)
 
     @property
     def events(self):
+        """
+        Returns events component
+        :rtype: SarasvatiEventsApiComponent
+        :return: Events component
+        """
         return self.get_component(SarasvatiEventsApiComponent.COMPONENT_NAME)
 
     @property
     def commands(self):
+        """
+        Returns commands component
+        :rtype: SarasvatiCommandsApiComponent
+        :return: Commands component
+        """
         return self.get_component(SarasvatiCommandsApiComponent.COMPONENT_NAME)
 
     def execute(self, command, transaction=None):
+        """
+        Executes command
+        :param command: String or Command instance
+        :param transaction: Transaction
+        :return: Command execution result
+        """
         return self.commands.execute(command, transaction)
 
     def open_brain(self, path):
+        """
+        Opens brain from specified path
+        :rtype: Brain
+        :param path: Path to oben brain from
+        :return: Brain
+        """
         logging.info("Opening brain from {}".format(path))
         storage_path = os.path.join(path, "db.json")
         storage_plugin = self.plugins.get("storage")
