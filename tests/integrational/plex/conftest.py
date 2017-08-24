028fb0387382ca648995c489a1b3e761d89e21c1
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-08-24 18:55:50 +0300

    tests cleanup

--- tests/integrational/plex/conftest.py
+++ tests/integrational/plex/conftest.py
@@ -4,23 +4,6 @@ from plugins.app.gui.plex import Plex, PlexLayout, PlexStateDiff
 from sarasvati.brain import Brain
 
 
-@pytest.fixture(name="storage")
-def __storage(api):
-    api.execute("/c Brain key:Brain")
-    api.execute("/c Tasks parent:Brain key:Tasks")
-    api.execute("/c Recipes parent:Brain key:Recipes")
-    api.execute("/c Read 'Alice in wunderland' parent:Tasks key:Task1")
-    api.execute("/c Cook cake parent:Tasks key:Task2")
-    api.execute("/c Anthill cake parent:Recipes key:Recipe1")
-    api.execute("/c Simple wounderful parent:Recipes key:Recipe2")
-    api.execute("/c Party key:Party")
-    api.execute("/c Guests parent:Party key:Guests")
-    api.execute("/l Cook cake to:Party as:child")
-    api.execute("/l Cook cake to:Anthill cake as:reference")
-
-    return api.storage
-
-
 @pytest.fixture(name="brain")
 def __brain(storage):
     return Brain(storage)
--- tests/integrational/storage/conftest.py
+++ tests/integrational/storage/conftest.py
@@ -9,22 +9,5 @@ def thought():
 
 
 @pytest.fixture
-def storage(api):
-    api.execute("/c Brain key:Brain")
-    api.execute("/c Tasks parent:Brain key:Tasks")
-    api.execute("/c Recipes parent:Brain key:Recipes")
-    api.execute("/c Read 'Alice in wunderland' parent:Tasks key:Task1")
-    api.execute("/c Cook cake parent:Tasks key:Task2")
-    api.execute("/c Anthill cake parent:Recipes key:Recipe1")
-    api.execute("/c Simple wounderful parent:Recipes key:Recipe2")
-    api.execute("/c Party key:Party")
-    api.execute("/c Guests parent:Party key:Guests")
-
-    api.storage.cache.clear()
-
-    return api.storage
-
-
-@pytest.fixture
 def empty_storage(api):
     return api.storage
