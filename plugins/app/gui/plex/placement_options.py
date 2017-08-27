308127f77a622c5ff588b2c55000c6de6db378e5
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-08-27 10:04:09 +0300

    PlexLayoutPlacement refactored

---
+++ plugins/app/gui/plex/placement_options.py
@@ -0,0 +1,6 @@
+class PlacementOptions:
+    def __init__(self):
+        """Initializes new instance of the PlacementOptions class."""
+        self.width = 275
+        self.height = 275
+        self.step = 50
